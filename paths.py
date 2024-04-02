603db5e584a9646cfc633b84a37494d268f81edc
= <=>
2024-04-02 19:26:54 +0200

    Updated paths

--- paths.py
+++ paths.py
@@ -1,3 +1,5 @@
+import os
+
 TFM_PATH = r"/home/anadal/Experiments/TFM"
 GIT_PATH = r"/home/anadal/Experiments/TFM/master_thesis"
 
@@ -8,5 +10,6 @@ MASKS_PATH = r""
 DETECTION_CSV = r"/home/anadal/Experiments/TFM/master_thesis/abnormal_detection_selection.csv"
 CLASSIFICATION_CSV = r"/home/anadal/Experiments/TFM/master_thesis/disease_classification_selection.csv"
 
-DETECTION_ENCODER = r"/home/anadal/Experiments/TFM/master_thesis/detection_encoder"
-CLASSIFICATION_ENCODER = r"/home/anadal/Experiments/TFM/master_thesis/classification_encoder"
+ENCODERS_PATH = r"/home/anadal/Experiments/TFM/master_thesis/encoders"
+DETECTION_ENCODER = os.path.join(ENCODERS_PATH, "detection_encoder")
+CLASSIFICATION_ENCODER = os.path.join(ENCODERS_PATH, "classification_encoder")
