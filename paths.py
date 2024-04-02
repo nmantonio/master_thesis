2cd784de02c1b81b0dee2ec4380e86aced4cf82c
nmantonio <nadalmantonio@gmail.com>
2024-04-02 19:39:40 +0200

    Reformulated paths

--- paths.py
+++ paths.py
@@ -1,15 +1,15 @@
 import os
 
 TFM_PATH = r"/home/anadal/Experiments/TFM"
-GIT_PATH = r"/home/anadal/Experiments/TFM/master_thesis"
+GIT_PATH = os.path.join(TFM_PATH, "master_thesis")
 
 RAW_DATABASE_PATH = r""
-DATABASE_PATH = r"/home/anadal/Experiments/TFM/PROCESSED_DATABASE"
+DATABASE_PATH = os.path.join(TFM_PATH, "PROCESSED_DATABASE")
 MASKS_PATH = r""
 
-DETECTION_CSV = r"/home/anadal/Experiments/TFM/master_thesis/abnormal_detection_selection.csv"
-CLASSIFICATION_CSV = r"/home/anadal/Experiments/TFM/master_thesis/disease_classification_selection.csv"
+DETECTION_CSV = os.path.join(GIT_PATH, "abnormal_detection_selection.csv")
+CLASSIFICATION_CSV = os.path.join(GIT_PATH, "disease_classification_selection.csv")
 
-ENCODERS_PATH = r"/home/anadal/Experiments/TFM/master_thesis/encoders"
+ENCODERS_PATH = os.path.join(GIT_PATH, "encoders")
 DETECTION_ENCODER = os.path.join(ENCODERS_PATH, "detection_encoder")
 CLASSIFICATION_ENCODER = os.path.join(ENCODERS_PATH, "classification_encoder")
