ea866f9d5ac1cb8558f87e038e49b8e76fdfc3df
= <=>
2024-04-02 19:23:50 +0200

    Added encoders object

---
+++ encoders/save_encoders.py
@@ -0,0 +1,34 @@
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import pickle
+from paths import *
+
+os.makedirs(ENCODERS_PATH, exist_ok=True)
+
+df = pd.read_csv(DETECTION_CSV)
+classification = pd.DataFrame(df["classification"])
+
+encoder = OneHotEncoder()
+encoder.fit(classification)
+print("--------- CLASSIFICATION ---------")
+print("IN: ", encoder.feature_names_in_)
+print("OUT: ", encoder.get_feature_names_out())
+
+with open(CLASSIFICATION_ENCODER, "wb") as f: 
+    pickle.dump(encoder, f)
+    
+del encoder
+
+print()
+# ------------------------------------------------------
+detection = pd.DataFrame(df["detection"])
+encoder = OneHotEncoder()
+encoder.fit(detection)
+print("--------- DETECTION ---------")
+print("IN: ", encoder.feature_names_in_)
+print("OUT: ", encoder.get_feature_names_out())
+
+with open(DETECTION_ENCODER, "wb") as f: 
+    pickle.dump(encoder, f)
+    
+del encoder
