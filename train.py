4c38685c81428c809bff8fb09ddd2fdc7db2ab86
nmantonio <nadalmantonio@gmail.com>
2024-04-03 10:14:09 +0200

    Reformulated train.py to fit current structure of files and added argparser

--- train.py
+++ train.py
@@ -1,77 +1,121 @@
+"""
+python train.py --model_name --fold_idx --task --save_path
+
+python train.py --model_name --fold_idx --task --save_path --trainable_core --batch_size --augmentation_prob
+"""
+
+
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
-import paths
+# os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
+import argparse
+
+from paths import *
 import pandas as pd
 import numpy as np
 
-import tensorflow as tf
-from tensorflow.keras.applications import Xception, MobileNetV2
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
-from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.callbacks import EarlyStopping, CSVLogger
-from tensorflow.keras.optimizers.schedules import ExponentialDecay
-# from metrics import F1Score
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from models.utils import modify_model, get_preprocessing_func
+from models.tops import *
+
+from keras.models import Model
+from keras.optimizers import Adam, SGD
+from keras.callbacks import EarlyStopping, CSVLogger
 
 from data_loader import get_dataset
 
-DATABASE_PATH = r"/home/anadal/Experiments/TFM/PROCESSED_DATABASE"
-TFM_PATH = r"/home/anadal/Experiments/TFM"
+parser = argparse
 
-DETECTION_CSV = r"/home/anadal/Experiments/TFM/master_thesis/abnormal_detection_selection.csv"
-# CLASSIFICATION_CSV = r"/home/anadal/Experiments/TFM/master_thesis/disease_classification_selection.csv"
+parser = argparse.ArgumentParser(description='Script for training a model')
 
-try_name = "Xception_adam_0.00001_abnormal_transfer"
-SAVE = os.path.join(r"/home/anadal/Experiments/TFM/master_thesis/Xception", try_name)
-os.makedirs(SAVE, exist_ok=True)
+parser.add_argument('--model_name', type=str, default='xception', help='Name of the model (default: xception)', choices=['xception', 'mobilenet'])
+parser.add_argument('--fold_idx', type=str, required=True, help='Fold index')
+parser.add_argument('--task', type=str, required=True, help='Type of task', choices=['detection', 'classification'])
+parser.add_argument('--save_path', type=str, required=True, help='Path to save the trained model')
+parser.add_argument('--trainable_core', type=bool, default=True, help='Whether to train the core of the model (default: True)')
+parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training (default: 32)')
+parser.add_argument('--augmentation_prob', type=float, default=0, help='Probability of augmentation (default: 0)')
 
-df = pd.read_csv(DETECTION_CSV)
-batch_size = 32
-FOLDS = ["2", "3", "4", "5"]
-fold_idx = "1"
+
+args = parser.parse_args()
+
+# Assigning argparse values to variables
+model_name = args.model_name
+trainable_core = args.trainable_core
+batch_size = args.batch_size
+fold_idx = args.fold_idx
+task = args.task
+augmentation_prob = args.augmentation_prob
+
+save_path = args.save_path
+os.makedirs(save_path)
+
+# Preprocessing
+preprocessing_func = get_preprocessing_func(name=model_name)
+
+# Read CSV
+if task == "detection":
+    df = pd.read_csv(DETECTION_CSV)
+    n_classes = 2
+elif task == "classification":
+    df = pd.read_csv(CLASSIFICATION_CSV)
+    n_classes = 7
+else: 
+    raise ValueError(f"{task} not available! Try 'detection' or 'classification'.")
+
+FOLDS = ["1", "2", "3", "4", "5"]
+FOLDS.remove(str(fold_idx))
 
 steps_per_epoch = np.ceil(df[df["split"].isin(FOLDS)].shape[0] / batch_size)
 val_steps_per_epoch = np.ceil(df[df["split"] == fold_idx].shape[0] / batch_size)
+
 print("STEPS PER EPOCH: ", steps_per_epoch)
 print("VAL STEPS PER EPOCH: ", val_steps_per_epoch)
 
 train_dataset = get_dataset(
     database_path=DATABASE_PATH,
-    csv_path = DETECTION_CSV, 
-    fold_idx=fold_idx,
-    batch_size=batch_size, 
-    mode="train",
-    repeat=True, 
-    augmentation=0
+    fold_idx=fold_idx, 
+    batch_size=batch_size,
+    mode="train", # train or val/test, 
+    task=task, # detection or classification
+    repeat=True,
+    preprocessing_func = preprocessing_func,
+    augmentation=augmentation_prob
 )
 
 val_dataset = get_dataset(
     database_path=DATABASE_PATH,
-    csv_path = DETECTION_CSV, 
-    fold_idx=fold_idx,
-    batch_size=batch_size, 
-    mode="test",
-    repeat=True, 
+    fold_idx=fold_idx, 
+    batch_size=batch_size,
+    mode="val", # train or val/test, 
+    task=task, # detection or classification
+    repeat=True,
+    preprocessing_func = preprocessing_func,
     augmentation=0
 )
 
-base_model = Xception(weights="imagenet", include_top=False, input_shape=(512, 512, 3))
+# base_model = Xception(weights=None, include_top=False, input_shape=(512, 512, 1))
+base_model = modify_model(name="xception", trainable=trainable_core)
 
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
-x = Dense(512, activation='relu')(x)
-x = Dense(256, activation='relu')(x)
-
-predictions = Dense(2, activation='softmax')(x)
+x = get_top(top_idx=1)(x)
+predictions = Dense(n_classes, activation="softmax")(x)
 
 # Create the fine-tuned model
-model = Model(inputs=base_model.input, outputs=predictions)
-model.summary()
+model = Model(inputs=base_model.input, outputs=predictions, name=model_name)
+model.summary(line_length=200)
 
 model.compile(optimizer=Adam(learning_rate=0.00001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(train_dataset, epochs=400, steps_per_epoch=steps_per_epoch, validation_data=val_dataset, validation_steps=val_steps_per_epoch, callbacks = [EarlyStopping(patience=30, restore_best_weights=True), CSVLogger(os.path.join(SAVE, "train_log.csv"))])
+model.fit(
+    train_dataset,
+    epochs=1,
+    steps_per_epoch=steps_per_epoch,
+    validation_data=val_dataset,
+    validation_steps=val_steps_per_epoch,
+    callbacks = [
+        EarlyStopping(patience=30, restore_best_weights=True),
+        CSVLogger(os.path.join(save_path, "train_log.csv"))
+    ]
+)
 
-model.save(os.path.join(SAVE, "model.h5"))
+model.save(os.path.join(save_path, "model.keras"))
