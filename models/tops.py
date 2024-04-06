6c0d7e030e38f73e5b41c6b92f29bf53e9ae00d1
nmantonio <nadalmantonio@gmail.com>
2024-04-06 18:47:12 +0200

    Reformulated tops

--- models/tops.py
+++ models/tops.py
@@ -4,8 +4,14 @@ from keras.layers import Dense, GlobalAveragePooling2D
 def get_top(top_idx):
     if int(top_idx) == 1: 
         return top_1
+    elif int(top_idx) == 2:
+        return top_2
 
 def top_1(x):
+    x = GlobalAveragePooling2D()(x)
+    return x
+
+def top_2(x):
     x = GlobalAveragePooling2D()(x)
     x = Dense(1024, activation='relu')(x)
     x = Dense(512, activation='relu')(x)
