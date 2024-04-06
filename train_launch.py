02e62d61a36d5c58cfa16fb77fbd1997db9a9908
nmantonio <nadalmantonio@gmail.com>
2024-04-06 18:04:39 +0200

    Changed excel saving to avoid removing second sheet

--- train_launch.py
+++ train_launch.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from paths import *
 
-device = "0"
+import argparse
+parser = argparse.ArgumentParser(description="Scrip to launch fold_train_and_val.py from excel sheet")
+parser.add_argument('--device', type=str, required=True, help='GPU device to use during training and validation')
+args = parser.parse_args()
+device = args.device
 
-df = pd.read_excel(TRAIN_SHEET)
+df = pd.read_excel(TRAIN_SHEET, sheet_name="trains")
 df["train_name"] = df["train_name"].astype(str)
 print(df)
 found = False
@@ -19,15 +23,17 @@ for idx, row in df.iterrows():
             train_name += f"{key}_{value}_"
         train_name = train_name[:-1]
         
-        # df.loc[idx, "done"] = "inprogress"
+        df.loc[idx, "done"] = "inprogress"
         df.loc[idx, "train_name"] = train_name
         train_data["train_name"] = train_name  
         train_data["device"] = device
 
       
         break
-    
-df.to_excel(TRAIN_SHEET, index=False, header=True)
+
+with pd.ExcelWriter(TRAIN_SHEET, engine='openpyxl', mode="a", if_sheet_exists="replace") as writer:
+    df.to_excel(writer, sheet_name="trains", index=False)
+# df.to_excel(TRAIN_SHEET, index=False, header=True, sheet_name="trains")
 del df
 # print(dict(row))
 # print(df)
@@ -40,18 +46,21 @@ if found:
         instruction += f' --{arg} {value}'
         
     print(instruction)
-    os.system(instruction)
+    # os.system(instruction)
 
 import json
 with open(os.path.join(TRAIN_PATH, train_name, "avg_results.json"), "r") as json_file:
     train_results = json.load(json_file)
 train_results["train_name"] = train_name
 results_df = pd.read_excel(RESULTS_SHEET)
-results_df = pd.concat([results_df, pd.DataFrame(data=train_results, index=[0])])
+results_df.loc[idx] = train_results
+# results_df = pd.concat([results_df, pd.DataFrame(data=train_results, index=[0])])
 results_df.to_excel(RESULTS_SHEET, index=False, header=True)
 del results_df
 
-df = pd.read_excel(TRAIN_SHEET)
+df = pd.read_excel(TRAIN_SHEET, sheet_name="trains")
 df.loc[idx, "done"] = "yes"
-df.to_excel(TRAIN_SHEET, index=False, header=True)
+with pd.ExcelWriter(TRAIN_SHEET, engine='openpyxl', mode="a", if_sheet_exists="replace") as writer:
+    df.to_excel(writer, sheet_name="trains", index=False)
+# df.to_excel(TRAIN_SHEET, index=False, header=True, sheet_name="trains")
 del df
